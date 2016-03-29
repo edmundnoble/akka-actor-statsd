b236dce6843fe2f59a85d89b918f61ae3673e487
Edmund Noble <edmundnoble@gmail.com>
2016-03-29 00:02:13 -0400

    Added more coverage to UnitStatsActorSpec

--- src/test/scala/deploymentzone/actor/unit/UnitStatsActorSpec.scala
+++ src/test/scala/deploymentzone/actor/unit/UnitStatsActorSpec.scala
@@ -14,6 +14,7 @@ class UnitStatsActorSpec
   "StatsActor" when {
     "using configuration-only props" should {
       "get initialized with the expected values when all values are specified" in {
+        import scala.concurrent.duration._
         val `stats-actor.conf` =
           """deploymentzone {
             |    akka-actor-statsd {
@@ -21,13 +22,15 @@ class UnitStatsActorSpec
             |        namespace = "mango"
             |        hostname = "127.0.0.1"
             |        packet-size = 100
-            |        transmit-interval = 100 ms
+            |        transmit-interval = 10000 ms
             |    }
             |}""".stripMargin
         val config = StatsConfig(ConfigFactory.parseString(`stats-actor.conf`))
         val subject = TestActorRef[StatsActor](StatsActor.props(config))
         subject.underlyingActor.config.address.getAddress.getHostAddress should be("127.0.0.1")
         subject.underlyingActor.config.address.getPort should be(9999)
+        subject.underlyingActor.config.transmitInterval should be(10000.millis)
+        subject.underlyingActor.config.packetSize should be(100)
         subject.underlyingActor.config.namespace should be("mango")
       }
     }
